247940ff4dcc87af3b22db446c8feeb817343938
vikasthakurr <vikaskumar20012001@gmail.com>
2026-02-27 18:35:43 +0530

    notes

---
+++ javascript/debounce-throttle.js
@@ -0,0 +1,33 @@
+// function searchWithDebounce(fn, delay) {
+//   let timer;
+//   return function (...args) {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       fn(...args);
+//     }, delay);
+//   };
+// }
+
+//throttle....
+
+function searchWithThrottle(fn, delay) {
+  let Lastcall = 0;
+  return function (...args) {
+    let currentCall = Date.now();
+    if (currentCall - Lastcall >= delay) {
+      fn(...args);
+      Lastcall = currentCall;
+    }
+  };
+}
+function search(name) {
+  console.log(`searching for ${name}`);
+}
+// search("vikas");
+// search("vikas singh");
+// search("vikas kumar thakur");
+
+// const searchInput = searchWithThrottle(search, 300);
+// searchInput("vikas");
+// searchInput("vikas singh");
+// searchInput("vikas kumar thakur");
--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-const p1 = Promise.reject("success1");
-const p2 = Promise.reject("success2");
-const p3 = Promise.reject("error");
+
